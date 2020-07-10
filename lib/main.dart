073f5c48a125efa1508e42f98ec49a2109d37768
Justin Bellero <jpbellero@gmail.com>
2020-07-10 18:40:17 -0400

    [UPDATE]: separate theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_trakr/new_transaction.dart';
+import 'package:flutter_expense_trakr/theme.dart';
+import 'package:flutter_expense_trakr/transaction_bloc.dart';
+import 'package:flutter_expense_trakr/transaction_model.dart';
 import 'package:flutter_expense_trakr/user_transactions.dart';
 
 void main() => runApp(MyApp());
@@ -8,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: appTheme(),
       home: MyHomePage(title: 'Personal Expenses'),
     );
   }
@@ -21,6 +22,15 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+  final TransactionController _txController = TransactionController();
+
+  void _startAddNewTrasaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return NewTransaction(_txController);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +38,18 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text(title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTrasaction(context),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTrasaction(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       body: SingleChildScrollView(
           child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -44,7 +65,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          UserTransactions(),
+          UserTransactions(Transaction.entries),
         ],
       )),
     );
---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() => ThemeData(
+      primarySwatch: Colors.green,
+      accentColor: Colors.greenAccent,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
